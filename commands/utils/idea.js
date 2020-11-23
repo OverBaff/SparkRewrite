1fce64d462febf641720072b1e5437174cb09ff4
RTXGalwet <ggalwet@gmail.com>
2020-11-23 22:48:59 +0300

    ебать идеа

--- commands/utils/idea.js
+++ commands/utils/idea.js
@@ -1,8 +1,9 @@
 const Idea = require('../../models/idea');
 module.exports = {
-    name: 'Idea',
-    args: true,
-    public: true,
-    run: async(message, args) => {
-    }
-}
+	name: 'Idea',
+	args: true,
+	public: true,
+	run: async (message, args) => {
+        
+	},
+};
